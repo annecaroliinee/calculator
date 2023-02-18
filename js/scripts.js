56875c815c44f8755b2d1eba29d51d93dccdeb2b
Anne Caroline Fonseca <annecarolineecosta@gmail.com>
2023-02-18 00:32:57 -0300

    inserindo const no aqrquivo JS

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
+
+class Calculator {
+  constructor(previousOperationText, currentOperationText) {
+    this.previousOperationText = previousOperationText;
+    this.currentOperationText = currentOperationText;
+    this.currentOperation = "";
+  }

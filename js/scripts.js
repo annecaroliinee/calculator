7b847f642d68e1bea3092ecc21e9f0bd5e5df0b7
Anne Caroline Fonseca <annecarolineecosta@gmail.com>
2023-02-18 15:41:59 -0300

    Add operações JS

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,65 @@ class Calculator {
     this.currentOperationText = currentOperationText;
     this.currentOperation = "";
   }
+
+  // adicionar dígito à tela da calculadora
+  addDigit(digit) {
+    console.log(digit);
+    // Verifique se o número já tem um ponto
+    if (digit === "." && this.currentOperationText.innerText.includes(".")) {
+      return;
+    }
+
+    this.currentOperation = digit;
+    this.updateScreen();
+  }
+
+  // processar todas as operações da calculadora
+  processOperation(operation) {
+    // Verifica se o valor atual está vazio
+    if (this.currentOperationText.innerText === "" && operation !== "C") {
+      // Mudar operação
+      if (this.previousOperationText.innerText !== "") {
+        this.changeOperation(operation);
+      }
+      return;
+    }
+
+    // Obtém valores atuais e anteriores
+    let operationValue;
+    let previous = +this.previousOperationText.innerText.split(" ")[0];
+    let current = +this.currentOperationText.innerText;
+
+    switch (operation) {
+      case "+":
+        operationValue = previous + current;
+        this.updateScreen(operationValue, operation, current, previous);
+        break;
+      case "-":
+        operationValue = previous - current;
+        this.updateScreen(operationValue, operation, current, previous);
+        break;
+      case "*":
+        operationValue = previous * current;
+        this.updateScreen(operationValue, operation, current, previous);
+        break;
+      case "/":
+        operationValue = previous / current;
+        this.updateScreen(operationValue, operation, current, previous);
+        break;
+      case "DEL":
+        this.processDelOperator();
+        break;
+      case "CE":
+        this.processClearCurrentOperator();
+        break;
+      case "C":
+        this.processClearOperator();
+        break;
+      case "=":
+        this.processEqualOperator();
+        break;
+      default:
+        return;
+    }
+  }
